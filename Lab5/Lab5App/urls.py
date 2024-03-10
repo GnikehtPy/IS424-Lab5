2b6839229bff7a5bd6b88f9b75358d7eb5740450
GnikehtPy <117049887+GnikehtPy@users.noreply.github.com>
2024-03-11 02:15:53 +0300

    Delete Lab5 directory

--- Lab5/Lab5App/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-app_name = "Lab5App"
-urlpatterns = [
-    path("", view=views.default, name="default"),
-    path("add", view=views.add, name="add")
-]
